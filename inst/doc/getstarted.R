5608f8a5b1b5af00a3762ac4ee70bd98ff929736
Marco Sciaini <marco.sciaini@posteo.net>
2021-09-20 07:30:05 +0000

    version 1.1

--- inst/doc/getstarted.R
+++ inst/doc/getstarted.R
@@ -1,12 +1,12 @@
-## ----global_options, include=FALSE---------------------------------------
+## ----global_options, include=FALSE--------------------------------------------
 library(raster)
 library(NLMR)
 
-## ---- fig.height=7, fig.width=7, fig.align='center'----------------------
+## ---- fig.height=7, fig.width=7, fig.align='center'---------------------------
 x <- NLMR::nlm_random(20,20)
 plot(x)
 
-## ---- fig.height=7, fig.width=7, fig.align='center'----------------------
+## ---- fig.height=7, fig.width=7, fig.align='center'---------------------------
   #Create primary landscape raster
   pL <- NLMR::nlm_edgegradient(ncol = 100,
                                nrow = 100)
@@ -24,7 +24,7 @@ plot(x)
   
   plot(mL1)
 
-## ----fig.height=7, fig.width=7, fig.align='center'-----------------------
+## ----fig.height=7, fig.width=7, fig.align='center'----------------------------
 nr <- NLMR::nlm_fbm(50, 100, fract_dim = 1.2)
                               
 nr_classified <- landscapetools::util_classify(nr, weighting = c(0.3, 0.3, 0.3))
